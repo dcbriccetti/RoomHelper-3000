810454a396cff75d18465ac042d7748b79725ce1
Dave Briccetti <daveb@davebsoft.com>
2018-08-18 11:14:08 -0700

    Read names from a file

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template, request
 from flask_socketio import SocketIO, emit, disconnect
 
+names = []
+with open('names.txt', 'r') as f:
+    for line in f.readlines():
+        names.append(line.strip())
+
 async_mode = None
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
@@ -9,11 +14,11 @@ socketio = SocketIO(app, async_mode=async_mode)
 
 @app.route('/')
 def index():
-    return render_template('index.html', async_mode=socketio.async_mode)
+    return render_template('index.html', names=names)
 
 @app.route('/admin')
 def admin():
-    return render_template('admin.html', async_mode=socketio.async_mode)
+    return render_template('admin.html', names=names)
 
 
 @socketio.on('seat')
