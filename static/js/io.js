fa15f7b214d7a414121264cd0a82ead58cb98bf0
Dave Briccetti <daveb@davebsoft.com>
2018-08-18 09:51:09 -0700

    Add a front of the room view

--- static/js/io.js
+++ static/js/io.js
@@ -1,11 +1,13 @@
 const rows = 4;
 const cols = 9;
-let seats = [ //new Array(rows * cols);
+const seats = [
     'A1', 'A2', 'A3', 'A4', 'A5', 'A6', 'A7', 'A8', '', 
     'B1', 'B2', 'B3', 'B4', 'B5', 'B6', 'B7', 'B8', 'B9', 
     'C1', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'C9',
     'D1', 'D2', 'D3', 'D4', 'D5', 'D6', 'D7', 'D8', '', 
 ];
+const missingSeatIndexes = new Set([8, 35]);
+const aisleAfterColumn = 3;
 
 $(document).ready(() => {
     const socket = io.connect();
--- static/js/sketch.js
+++ static/js/sketch.js
@@ -1,38 +1,41 @@
 function setup() {
     createCanvas(800, 400).parent('container');
-    frameRate(1);
+    frameRate(2);
 }
 
 function draw() {
     const aisle = 7;
-    const w = (width - aisle) / 9;
-    const h = height / 4;
+    const w = (width - aisle) / cols;
+    const h = height / rows;
+    const frontView = $('#front-view').is(':checked');
 
-    function drawGroup(startX, startY, startSeatIndex, num) {
+    function drawGroup(startX, startY, seatIndex) {
         textFont('Helvetica');
         textSize(14);
         strokeWeight(1);
-        for (let i = 0; i < num; ++i) {
-            const x = startX + i * w;
-            noStroke();
-            fill(136, 159, 178);
-            rect(x, startY, w - 3, h - 3);
-            const name = seats[startSeatIndex + i];
-            if (name) {
-                noStroke();
-                fill(0);
-                text(name, x + 5, startY + 10, w, h);
-            }
-        }
+        noStroke();
+        fill(136, 159, 178);
+        rect(startX, startY, w - 3, h - 3);
+        const name = seats[seatIndex];
+        noStroke();
+        fill(0);
+        text(name, startX + 4, startY + 5, w, h);
     }
 
     background(255);
 
-    const x1 = 0;
-    const x2 = w * 4 + aisle;
-    [4, 5, 5, 4].forEach((cols, i) => {
-        drawGroup(x1, h * i, i * 9, 4);
-        drawGroup(x2, h * i, 4 + 9 * i, cols);
-    });
+    for (let r = 0; r < rows; ++r) {
+        for (let c = 0; c < cols; ++c) {
+            const seatIndex = r * 9 + c;
+            if (!missingSeatIndexes.has(seatIndex)) {
+                const ar = frontView ? rows - 1 - r : r;
+                const ac = frontView ? cols - 1 - c : c;
+                const aisleAdj = frontView ?
+                    ac > cols - 1 - 1 - aisleAfterColumn ? aisle : 0 :
+                    ac > aisleAfterColumn ? aisle : 0;
+                drawGroup(ac * w + aisleAdj, ar * h, seatIndex);
+            }
+        }
+    }
 }
 
