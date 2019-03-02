184ee0b7f4ac4f3d4606e837dcc000f2e5cc2151
Dave Briccetti <daveb@davebsoft.com>
2019-03-02 09:27:11 -0800

    Simplify station location processing

--- static/js/sketch.js
+++ static/js/sketch.js
@@ -1,8 +1,18 @@
 'use strict';
 
+class StationLoc {
+    constructor(index, x, y) {
+        this.index = index;
+        this.x = Math.round(x);
+        this.y = y;
+    }
+}
+
 const sketch = new p5(p => {
 
-    p.buildStationDrawingLocations = function() {
+    const AISLE_WIDTH = 7;
+
+    p.getStationLocs = function() {
         const missingSeatIndexes = new Set(settings.missingSeatIndexes);
         const frontView = $('#front-view').is(':checked');
         const aisleWidth = 7;
@@ -21,35 +31,42 @@ const sketch = new p5(p => {
 
         function adjustedY(r) {return (frontView ? settings.rows - 1 - r : r) * h;}
 
-        const stationDrawLocations = [];
+        const locs = [];
 
         for (let r = 0; r < settings.rows; ++r) {
             for (let c = 0; c < settings.columns; ++c) {
                 const seatIndex = r * settings.columns + c;
                 if (!missingSeatIndexes.has(seatIndex)) {
-                    stationDrawLocations.push([adjustedX(r, c), adjustedY(r), seatIndex]);
+                    locs.push(new StationLoc(seatIndex, adjustedX(r, c), adjustedY(r)));
                 }
             }
         }
-        return stationDrawLocations;
+        return locs;
     };
 
-    p.stationDrawLocations = [];
+    p.stationLocs = [];
 
     p.setup = function() {
-        p.createCanvas(800, 400).parent('canvas');
+        p.createCanvas(800, 400).parent('canvas-container');
         p.reconfigure();
     };
 
     p.reconfigure = function() {
-        p.stationDrawLocations = p.buildStationDrawingLocations();
+        p.stationLocs = p.getStationLocs();
         p.loop();
     };
 
+    function stationWidth() {
+        return (p.width - AISLE_WIDTH) / settings.columns;
+    }
+
+    function stationHeight() {
+        return p.height / settings.rows;
+    }
+
     p.draw = function() {
-        const aisleWidth = 7;
-        const w = (p.width - aisleWidth) / settings.columns;
-        const h = p.height / settings.rows;
+        const w = stationWidth();
+        const h = stationHeight();
         const normalColor   = [168, 196, 219];
         const selectedColor = [230, 230, 230];
 
@@ -59,20 +76,20 @@ const sketch = new p5(p => {
             return String.fromCharCode('A'.charCodeAt(0) + rowFrom0) + (colFrom0 + 1);
         }
 
-        function drawStation(startX, startY, seatIndex) {
+        function drawStation(loc) {
 
             function drawStatusTags(name, y, xMargin, xPerKey, tagHeight) {
                 const tagColors = [[45, 98, 163], [228, 113, 39], [142, 145, 143]];
                 settings.statuses.forEach((s, i) => {
                     const key = s[0];
                     const code = s[1];
-                    const x = startX + xMargin + xPerKey * i;
+                    const x = loc.x + xMargin + xPerKey * i;
                     const keyOrder = status.orders[key];
                     if (keyOrder) {
                         const studentOrder = keyOrder[name];
                         if (studentOrder) {
                             p.fill(tagColors[i % tagColors.length]);
-                            p.rect(x - 2, startY + h - tagHeight - 3, xPerKey, tagHeight);
+                            p.rect(x - 2, loc.y + h - tagHeight - 3, xPerKey, tagHeight);
                             p.fill(0);
                             p.text(code + studentOrder.order, x, y);
                         }
@@ -82,38 +99,38 @@ const sketch = new p5(p => {
 
             p.textFont('Helvetica');
             p.noStroke();
-            const station = stations[seatIndex];
-            p.fill(seatIndex === selectedSeatIndex ? selectedColor : normalColor);
-            p.rect(startX, startY, w - 3, h - 3);
+            const station = stations[loc.index];
+            p.fill(loc.index === selectedSeatIndex ? selectedColor : normalColor);
+            p.rect(loc.x, loc.y, w - 3, h - 3);
 
             p.fill(0);
             const xMargin = 2;
             p.textSize(10);
             p.textAlign(p.LEFT, p.TOP);
-            p.text(stationName(seatIndex), startX + xMargin, startY + 3);
+            p.text(stationName(loc.index), loc.x + xMargin, loc.y + 3);
 
             if (station && station.name) {
                 p.fill(station.connected ? 0 : 128);
                 p.textAlign(p.RIGHT);
-                p.text(station.ip, startX + w - xMargin - 2 /* todo why this 2 */, startY + 3);
+                p.text(station.ip, loc.x + w - xMargin - 2 /* todo why this 2 */, loc.y + 3);
                 const parts = station.name.split(', ');
                 p.textSize(20);
                 p.textAlign(p.CENTER, p.CENTER);
                 const name = parts[1];
                 if (p.textWidth(name) > w) p.textSize(14);
-                p.text(name, startX + w / 2, startY + h / 3);
+                p.text(name, loc.x + w / 2, loc.y + h / 3);
                 p.textSize(12);
-                p.text(parts[0], startX + w / 2, startY + h / 3 + 20);
+                p.text(parts[0], loc.x + w / 2, loc.y + h / 3 + 20);
 
                 p.textSize(14);
                 p.textAlign(p.LEFT, p.BOTTOM);
                 const xPerKey = (w - 2 * xMargin) / status.keys.length;
                 const tagHeight = 1 / 3 * h;
 
-                const y = startY + h - 4;
+                const y = loc.y + h - 4;
                 const answer = station.answer;
                 if (showAnswersInStations && answer) {
-                    p.text(answer, startX + xMargin, y);
+                    p.text(answer, loc.x + xMargin, y);
                 } else {
                     drawStatusTags(station.name, y, xMargin, xPerKey, tagHeight);
                 }
@@ -122,14 +139,13 @@ const sketch = new p5(p => {
 
         p.background(255);
 
-        p.stationDrawLocations.forEach(loc => drawStation(loc[0], loc[1], loc[2]));
+        p.stationLocs.forEach(loc => drawStation(loc));
 
         p.noLoop();
     };
 
-    p.mouseClicked = function() {
-       p.stationDrawLocations.forEach(loc => {
-           // TODO: console.log(loc, p.mouseX, p.mouseY);
-       });
+    p.doubleClicked = function () {
+        p.stationLocs.forEach(loc => {
+        });
     };
  });
