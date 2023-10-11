1a68df3f8ed46324a4ad99500afd7073ebcdae8e
Dave Briccetti <daveb@davebsoft.com>
2023-10-11 02:31:38 -0700

    Get a failing import working

--- app/src/io-util.ts
+++ app/src/io-util.ts
@@ -1,9 +1,8 @@
-// todo get following import to work. error: Uncaught TypeError: The specifier “socket.ioUtil-client” was a bare specifier, but was not remapped to anything. Relative module specifiers must start with “./”, “../” or “/”.
-// import {io, Socket} from "socket.io-client"
-
-declare const io
+import {io} from "socket.io-client"
 
 export function newSocket(channel: string) {
+    alert(`Connecting to ${channel}`)
+    console.log(`Connecting to ${channel}`)
     const protocol = window.location.protocol
     const port = window.location.port || (protocol === 'https:' ? '443' : '80')
     const uri = `${window.location.protocol}//${window.location.hostname}:${port}/${channel}`
